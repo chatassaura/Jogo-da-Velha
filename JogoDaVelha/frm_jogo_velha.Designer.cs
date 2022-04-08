192910215c9d7bc06037169b5e5d63ba4d81d767
Fernanda Martins <71784710+chatassaura@users.noreply.github.com>
2022-04-08 19:54:54 -0300

    Add files via upload

---
+++ JogoDaVelha/frm_jogo_velha.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace JogoDaVelha
+{
+    partial class frm_jogo_velha
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_jogo_velha));
+            this.img_grid = new System.Windows.Forms.PictureBox();
+            this.img_a1 = new System.Windows.Forms.PictureBox();
+            this.img_b3 = new System.Windows.Forms.PictureBox();
+            this.img_a3 = new System.Windows.Forms.PictureBox();
+            this.img_a2 = new System.Windows.Forms.PictureBox();
+            this.img_b2 = new System.Windows.Forms.PictureBox();
+            this.img_b1 = new System.Windows.Forms.PictureBox();
+            this.img_c1 = new System.Windows.Forms.PictureBox();
+            this.img_c3 = new System.Windows.Forms.PictureBox();
+            this.img_c2 = new System.Windows.Forms.PictureBox();
+            this.img_resultado = new System.Windows.Forms.PictureBox();
+            this.lbl_o = new System.Windows.Forms.Label();
+            this.lbl_x = new System.Windows.Forms.Label();
+            this.lbl_score = new System.Windows.Forms.Label();
+            this.btn_reset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.img_grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_a1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_b3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_a3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_a2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_b2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_b1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_c1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_c3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_c2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_resultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // img_grid
+            // 
+            this.img_grid.BackColor = System.Drawing.Color.Transparent;
+            this.img_grid.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.img_grid.Image = ((System.Drawing.Image)(resources.GetObject("img_grid.Image")));
+            this.img_grid.Location = new System.Drawing.Point(0, 0);
+            this.img_grid.Name = "img_grid";
+            this.img_grid.Size = new System.Drawing.Size(484, 452);
+            this.img_grid.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_grid.TabIndex = 0;
+            this.img_grid.TabStop = false;
+            // 
+            // img_a1
+            // 
+            this.img_a1.BackColor = System.Drawing.Color.Transparent;
+            this.img_a1.Location = new System.Drawing.Point(24, 24);
+            this.img_a1.Name = "img_a1";
+            this.img_a1.Size = new System.Drawing.Size(102, 102);
+            this.img_a1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_a1.TabIndex = 1;
+            this.img_a1.TabStop = false;
+            this.img_a1.Click += new System.EventHandler(this.jogar_click);
+            // 
+            // img_b3
+            // 
+            this.img_b3.BackColor = System.Drawing.Color.Transparent;
+            this.img_b3.Location = new System.Drawing.Point(346, 172);
+            this.img_b3.Name = "img_b3";
+            this.img_b3.Size = new System.Drawing.Size(102, 102);
+            this.img_b3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_b3.TabIndex = 2;
+            this.img_b3.TabStop = false;
+            this.img_b3.Click += new System.EventHandler(this.jogar_click);
+            // 
+            // img_a3
+            // 
+            this.img_a3.BackColor = System.Drawing.Color.Transparent;
+            this.img_a3.Location = new System.Drawing.Point(346, 24);
+            this.img_a3.Name = "img_a3";
+            this.img_a3.Size = new System.Drawing.Size(102, 102);
+            this.img_a3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_a3.TabIndex = 3;
+            this.img_a3.TabStop = false;
+            this.img_a3.Click += new System.EventHandler(this.jogar_click);
+            // 
+            // img_a2
+            // 
+            this.img_a2.BackColor = System.Drawing.Color.Transparent;
+            this.img_a2.Location = new System.Drawing.Point(191, 24);
+            this.img_a2.Name = "img_a2";
+            this.img_a2.Size = new System.Drawing.Size(102, 102);
+            this.img_a2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_a2.TabIndex = 4;
+            this.img_a2.TabStop = false;
+            this.img_a2.Click += new System.EventHandler(this.jogar_click);
+            // 
+            // img_b2
+            // 
+            this.img_b2.BackColor = System.Drawing.Color.Transparent;
+            this.img_b2.Location = new System.Drawing.Point(191, 174);
+            this.img_b2.Name = "img_b2";
+            this.img_b2.Size = new System.Drawing.Size(102, 102);
+            this.img_b2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_b2.TabIndex = 5;
+            this.img_b2.TabStop = false;
+            this.img_b2.Click += new System.EventHandler(this.jogar_click);
+            // 
+            // img_b1
+            // 
+            this.img_b1.BackColor = System.Drawing.Color.Transparent;
+            this.img_b1.Location = new System.Drawing.Point(24, 174);
+            this.img_b1.Name = "img_b1";
+            this.img_b1.Size = new System.Drawing.Size(102, 102);
+            this.img_b1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_b1.TabIndex = 6;
+            this.img_b1.TabStop = false;
+            this.img_b1.Click += new System.EventHandler(this.jogar_click);
+            // 
+            // img_c1
+            // 
+            this.img_c1.BackColor = System.Drawing.Color.Transparent;
+            this.img_c1.Location = new System.Drawing.Point(24, 336);
+            this.img_c1.Name = "img_c1";
+            this.img_c1.Size = new System.Drawing.Size(102, 102);
+            this.img_c1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_c1.TabIndex = 7;
+            this.img_c1.TabStop = false;
+            this.img_c1.Click += new System.EventHandler(this.jogar_click);
+            // 
+            // img_c3
+            // 
+            this.img_c3.BackColor = System.Drawing.Color.Transparent;
+            this.img_c3.Location = new System.Drawing.Point(346, 336);
+            this.img_c3.Name = "img_c3";
+            this.img_c3.Size = new System.Drawing.Size(102, 102);
+            this.img_c3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_c3.TabIndex = 8;
+            this.img_c3.TabStop = false;
+            this.img_c3.Click += new System.EventHandler(this.jogar_click);
+            // 
+            // img_c2
+            // 
+            this.img_c2.BackColor = System.Drawing.Color.Transparent;
+            this.img_c2.Location = new System.Drawing.Point(191, 336);
+            this.img_c2.Name = "img_c2";
+            this.img_c2.Size = new System.Drawing.Size(102, 102);
+            this.img_c2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_c2.TabIndex = 9;
+            this.img_c2.TabStop = false;
+            this.img_c2.Click += new System.EventHandler(this.jogar_click);
+            // 
+            // img_resultado
+            // 
+            this.img_resultado.BackColor = System.Drawing.Color.Transparent;
+            this.img_resultado.Location = new System.Drawing.Point(0, 0);
+            this.img_resultado.Name = "img_resultado";
+            this.img_resultado.Size = new System.Drawing.Size(484, 450);
+            this.img_resultado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_resultado.TabIndex = 20;
+            this.img_resultado.TabStop = false;
+            // 
+            // lbl_o
+            // 
+            this.lbl_o.AutoSize = true;
+            this.lbl_o.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_o.Font = new System.Drawing.Font("Perpetua Titling MT", 15.75F, System.Drawing.FontStyle.Bold);
+            this.lbl_o.Location = new System.Drawing.Point(315, 471);
+            this.lbl_o.Name = "lbl_o";
+            this.lbl_o.Size = new System.Drawing.Size(64, 26);
+            this.lbl_o.TabIndex = 26;
+            this.lbl_o.Text = "O - 0";
+            // 
+            // lbl_x
+            // 
+            this.lbl_x.AutoSize = true;
+            this.lbl_x.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_x.Font = new System.Drawing.Font("Perpetua Titling MT", 15.75F, System.Drawing.FontStyle.Bold);
+            this.lbl_x.Location = new System.Drawing.Point(222, 471);
+            this.lbl_x.Name = "lbl_x";
+            this.lbl_x.Size = new System.Drawing.Size(64, 26);
+            this.lbl_x.TabIndex = 25;
+            this.lbl_x.Text = "X - 0";
+            // 
+            // lbl_score
+            // 
+            this.lbl_score.AutoSize = true;
+            this.lbl_score.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_score.Font = new System.Drawing.Font("Perpetua Titling MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_score.Location = new System.Drawing.Point(46, 471);
+            this.lbl_score.Name = "lbl_score";
+            this.lbl_score.Size = new System.Drawing.Size(161, 26);
+            this.lbl_score.TabIndex = 24;
+            this.lbl_score.Text = "Resultado: ";
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.AutoSize = true;
+            this.btn_reset.BackColor = System.Drawing.Color.Transparent;
+            this.btn_reset.Image = ((System.Drawing.Image)(resources.GetObject("btn_reset.Image")));
+            this.btn_reset.Location = new System.Drawing.Point(411, 470);
+            this.btn_reset.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(37, 35);
+            this.btn_reset.TabIndex = 27;
+            this.btn_reset.UseVisualStyleBackColor = false;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // frm_jogo_velha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(484, 514);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.lbl_o);
+            this.Controls.Add(this.lbl_x);
+            this.Controls.Add(this.lbl_score);
+            this.Controls.Add(this.img_c2);
+            this.Controls.Add(this.img_c3);
+            this.Controls.Add(this.img_c1);
+            this.Controls.Add(this.img_b1);
+            this.Controls.Add(this.img_b2);
+            this.Controls.Add(this.img_a2);
+            this.Controls.Add(this.img_a3);
+            this.Controls.Add(this.img_b3);
+            this.Controls.Add(this.img_a1);
+            this.Controls.Add(this.img_grid);
+            this.Controls.Add(this.img_resultado);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frm_jogo_velha";
+            this.Text = "Jogo da Velha";
+            ((System.ComponentModel.ISupportInitialize)(this.img_grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_a1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_b3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_a3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_a2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_b2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_b1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_c1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_c3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_c2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_resultado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox img_grid;
+        private System.Windows.Forms.PictureBox img_a1;
+        private System.Windows.Forms.PictureBox img_b3;
+        private System.Windows.Forms.PictureBox img_a3;
+        private System.Windows.Forms.PictureBox img_a2;
+        private System.Windows.Forms.PictureBox img_b2;
+        private System.Windows.Forms.PictureBox img_b1;
+        private System.Windows.Forms.PictureBox img_c1;
+        private System.Windows.Forms.PictureBox img_c3;
+        private System.Windows.Forms.PictureBox img_c2;
+        private System.Windows.Forms.PictureBox img_resultado;
+        private System.Windows.Forms.Label lbl_o;
+        private System.Windows.Forms.Label lbl_x;
+        private System.Windows.Forms.Label lbl_score;
+        private System.Windows.Forms.Button btn_reset;
+    }
+}
+
---
+++ JogoDaVelha/frm_jogo_velha.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaVelha
+{
+    public partial class frm_jogo_velha : Form
+    {
+        int score_X = 0;
+        int score_O = 0;
+        bool ganhou = false;
+        int count_player = 0;
+        string[,] matriz = new string[3, 3];
+        string simbolo = "";
+
+        public frm_jogo_velha()
+        {
+            InitializeComponent();
+            img_grid.Parent = img_resultado;
+            btn_reset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btn_reset.FlatAppearance.BorderSize = 0;
+            btn_reset.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            btn_reset.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            btn_reset.BackColor = Color.Transparent;
+        }
+
+        private void VerificarGanhou(string xo)
+        {
+            // verificar se ganhou nas linhas 
+            for (int i = 0; i < 3; i++)
+            {
+                if (matriz[i, 0] == xo && matriz[i, 1] == xo && matriz[i, 2] == xo)
+                {
+                    ganhou = true;
+                    img_resultado.Image = Image.FromFile("l"+(i+1)+".png");
+                    mensagem_final(xo);
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (matriz[0, i] == xo && matriz[1, i] == xo && matriz[2, i] == xo)
+                {
+                    ganhou = true;
+                    img_resultado.Image = Image.FromFile("c" + (i + 1) + ".png");
+                    mensagem_final(xo);
+                }
+            }
+
+            if (matriz[0, 0] == xo && matriz[1, 1] == xo && matriz[2, 2] == xo)
+            {
+                ganhou = true;
+                img_resultado.Image = Image.FromFile("v2.png");
+                mensagem_final(xo);
+            }
+            if (matriz[0, 2] == xo && matriz[1, 1] == xo && matriz[2, 0] == xo)
+            {
+                ganhou = true;
+                img_resultado.Image = Image.FromFile("v1.png"); 
+                mensagem_final(xo);
+            }
+            if(count_player == 9 && !ganhou )
+            {
+                mensagem_final(xo);
+            }
+        }
+
+        private void jogar_click(object sender, EventArgs e)
+        {
+            PictureBox px = (PictureBox)sender;
+            px.Parent = img_resultado;
+            px.BringToFront();
+
+            if (px.Image == null) {
+                if (count_player % 2 == 0)
+                {  
+                    px.Image = Image.FromFile("bola.png");
+                    simbolo = "O";
+                }
+                else
+                {
+                    px.Image = Image.FromFile("xis.png");
+                    simbolo = "X";
+                }
+
+                count_player++;
+                registrarNaMatriz(px, simbolo);
+                VerificarGanhou(simbolo);
+            }
+        }
+
+
+        public void registrarNaMatriz(PictureBox px, string simbolo)
+        {
+
+            if (px.Name.Equals("img_a1"))
+            {
+                matriz[0, 0] = simbolo;
+            } 
+            else if (px.Name.Equals("img_a2")){
+                matriz[0, 1] = simbolo;
+            }
+            else if (px.Name.Equals("img_a3")){
+                matriz[0, 2] = simbolo;
+            }
+            else if (px.Name.Equals("img_b1")){
+                matriz[1, 0] = simbolo;
+            }
+            else if(px.Name.Equals("img_b2")){
+                matriz[1, 1] = simbolo;
+            }
+            else if (px.Name.Equals("img_b3")){
+                matriz[1, 2] = simbolo;
+            }
+            else if (px.Name.Equals("img_c1")){
+                matriz[2, 0] = simbolo;
+            }
+            else if (px.Name.Equals("img_c2")){
+                matriz[2, 1] = simbolo;
+            }
+            else {
+                matriz[2, 2] = simbolo;
+            }
+        }
+
+        public void resetar_jogo()
+        {
+            img_a1.Image = null;
+            img_a2.Image = null;
+            img_a3.Image = null;
+            img_b1.Image = null;
+            img_b2.Image = null;
+            img_b3.Image = null;
+            img_c1.Image = null;
+            img_c2.Image = null;
+            img_c3.Image = null;
+            img_resultado.Image = null;
+            ganhou = false;
+            count_player = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matriz[i, j] = "";
+                }
+            }   
+        }
+        public void mensagem_final(string simbolo)
+        {
+            var final = DialogResult.No;
+            if (ganhou == true)
+            {
+                if (simbolo == "X")
+                {
+                    score_X++;
+                    lbl_x.Text = "X - " + score_X.ToString();
+                }
+                else
+                {
+                    score_O++;
+                    lbl_o.Text = "O - " + score_O.ToString();
+
+                }
+                final = MessageBox.Show("O Jogador " + simbolo + " Ganhou! ", "fim de jogo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            }
+            else
+            {
+                final = MessageBox.Show("Deu Velha!", "fim de jogo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            }
+                if (final == DialogResult.Yes)
+            {
+                resetar_jogo();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            resetar_jogo();
+            score_X = 0;
+            score_O = 0;
+            lbl_x.Text = "X - " + score_X.ToString();
+            lbl_o.Text = "O - " + score_O.ToString();
+        }
+    }
+}
